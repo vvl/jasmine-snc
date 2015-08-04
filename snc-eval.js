46bf4eebec55a3bf612e89e619ea5df211824bd1
viktor.v.lukashov@gmail.com <viktor.v.lukashov+github@gmail.com>
2015-08-04 06:04:49 +0000

    snc-eval can be run both standalone and from another code

--- snc-eval.js
+++ snc-eval.js
@@ -3,7 +3,12 @@
 var fs = require('fs');
 var request = require("request");
 
-function PostCode(codestring) {
+function PostCode(codestring, callback) {
+  var done = function(error, result) {};
+  if (typeof callback == 'function') {
+    done = callback;
+  }
+  
   var username = 'jasmine-snc';
   var password = 'jasmine-snc';
   var instance_url = 'https://demo022.service-now.com/';
@@ -13,40 +18,57 @@ function PostCode(codestring) {
       baseUrl: instance_url,
       url: '/eval.do',
       form: { script: codestring },
-      auth: { user: username, pass: password }
+      auth: { user: username, pass: password },
+      timeout: 5000
     },
     function(error, response, body) {
       if (error) {
-        console.log('error: ' + error)
+        done(error.toString(), null);
         return;
       }
       
       if (response.statusCode == 200) {
-        console.log(body);
+        done(null, body);
       } else {
-        console.log(JSON.stringify(response));
+        done(JSON.stringify(response), null);
       }
     }
   );
 }
 
-
-console.log("snc-eval - run Javascript code in a ServiceNow backend");
-if (process.argv.length < 3) {
-  console.log("no input file is specified -> exiting");
-  process.exit(-1);
+function SnvEvalFile(filename, callback) {
+  var done = function(error, result) {};
+  if (typeof callback == 'function') {
+    done = callback;
+  }
+  
+  fs.readFile(filename, 'utf-8', function(error, data) {
+    if (error) {
+      done("FATAL An error occurred trying to read in the file: " + error, null);
+    } else if (data) {
+      PostCode(data, callback);
+    } else {
+      done("the input file is empty -> existing", null);
+    }
+  });
 }
 
-fs.readFile(process.argv[2], 'utf-8', function(err, data) {
-  if (err) {
-    console.log("FATAL An error occurred trying to read in the file: " + err);
-    process.exit(-2);
+// the code below only executes when this file is run directly
+// it does not execute when this file is require()'d from another file
+// this allows includuing this file as a module to other files
+if (require.main === module) {
+  console.log("snc-eval - run Javascript code in a ServiceNow backend");
+  if (process.argv.length < 3) {
+    console.log("no input file is specified -> exiting");
+    process.exit(-1);
   }
-  if (data) {
-    PostCode(data);
-  }
-  else {
-    console.log("the input file is empty -> existing");
-    process.exit(0);
-  }
-});
+  
+  SnvEvalFile(process.argv[2], function(error, result) {
+    if (error) {
+      console.log(error);
+      process.exit(-2);
+    } else {
+      console.log(result);
+    }
+  });
+}
