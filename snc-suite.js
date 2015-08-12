cf7407649fcce49f741c832b687e08a79a3ba5f0
viktor.v.lukashov@gmail.com <viktor.v.lukashov+github@gmail.com>
2015-08-12 04:31:36 +0000

    added elapsed time and crash count to summary

--- snc-suite.js
+++ snc-suite.js
@@ -6,21 +6,31 @@ var snc_eval = require("./snc-eval");
 // Jasmine "runner"
 function executeSpecs(specs, done, isVerbose, showColors) {
   var results = [];
+  var started = new Date().getTime();
   
   function printResultSummary() {
+    var elapsed = new Date().getTime() - started;
     console.log("\nSummary of " + results.length + " file(s):");
     var totalSpecs = 0,
-        totalFailures = 0;
+        totalFailures = 0,
+        totalCrashes = 0,
+        totalTimeInSnc = 0.0;
     results.forEach(function(result) {
       var summary = 'CRASHED';
       if (result.status == 'OK') {
-        summary = result.counts.specs + ' specs, ' + result.counts.failures + ' failures';
-        totalSpecs += parseInt(result.counts.specs);
-        totalFailures += parseInt(result.counts.failures);
+        summary = result.counts.specs + ' specs, ' + result.counts.failures + ' failures, ' +
+          (result.counts.pending > 0 ? (result.counts.pending + ' pending, ') : '') +
+          result.counts.time + ' seconds';
+        totalSpecs += result.counts.specs;
+        totalFailures += result.counts.failures;
+        totalTimeInSnc += result.counts.time;
+      } else {
+        totalCrashes += 1;
       }
       console.log(result.filename + ': ' + summary);
     });
-    console.log(totalSpecs + " specs, " + totalFailures + " failures");
+    console.log(results.length + " files, " + totalCrashes + " crashed, ", totalSpecs + " specs, " + totalFailures + " failures");
+    console.log('Total time ' + elapsed / 1000 + ' seconds, ServiceNow time ' + totalTimeInSnc + ' seconds');
   }
   
   var filefn = function(idx, next) {
@@ -39,9 +49,18 @@ function executeSpecs(specs, done, isVerbose, showColors) {
           var result = JSON.parse(body);
           if (result.status == 'OK') {
             console.log(result.output);
-            var re = /(\d+) specs?, (\d+) failures?\n*Finished in \d+\.\d+ seconds?\n?$/;
+            var re = /(\d+) specs?, (\d+) failures?(, (\d+) pending specs?)?\n*Finished in (\d+\.\d+) seconds?\n?$/;
             var matches = re.exec(result.output);
-            results.push({filename: filename, status: result.status, counts: { specs: matches[1], failures: matches[2] }});
+            results.push({
+              filename: filename,
+              status: result.status,
+              counts: {
+                specs: parseInt(matches[1]),
+                failures: parseInt(matches[2]),
+                pending: typeof matches[4] === 'undefined' ? 0 : parseInt(matches[4]),
+                time: parseFloat(matches[5])
+              }
+            });
           } else {
             console.log('ERROR:');
             console.log(result.error);
@@ -81,7 +100,7 @@ function getFiles(dir, matcher) {
 }
 
 function getSpecFiles(dir) {
-  return getFiles(dir, new RegExp("EnvSpec.js$"));
+  return getFiles(dir, new RegExp("\.js$"));
 }
 
 // options from command line
